915c132b5c59155f09ade6db018c2c28156c696e
Daniel Bacsur <contact@danielbacsur.com>
2024-03-04 16:16:58 +0100

    feat: add laser scan topics and virtual laser cloud parameters

--- src/virtual_laser_cloud.cpp
+++ src/virtual_laser_cloud.cpp
@@ -8,9 +8,29 @@ class VirtualLaserCloud : public rclcpp::Node
 public:
     VirtualLaserCloud() : Node("virtual_laser_cloud")
     {
+        this->declare_parameter<std::string>("front_left_laser_scan_topic", "front_left_laser/scan");
+        this->get_parameter_or<std::string>("front_left_laser_scan_topic", front_left_laser_scan_topic_, "front_left_laser/scan");
+        this->declare_parameter<std::string>("front_right_laser_scan_topic", "front_right_laser/scan");
+        this->get_parameter_or<std::string>("front_right_laser_scan_topic", front_right_laser_scan_topic_, "front_right_laser/scan");
+        this->declare_parameter<std::string>("rear_left_laser_scan_topic", "rear_left_laser/scan");
+        this->get_parameter_or<std::string>("rear_left_laser_scan_topic", rear_left_laser_scan_topic_, "rear_left_laser/scan");
+        this->declare_parameter<std::string>("rear_right_laser_scan_topic", "rear_right_laser/scan");
+        this->get_parameter_or<std::string>("rear_right_laser_scan_topic", rear_right_laser_scan_topic_, "rear_right_laser/scan");
+
+        this->declare_parameter<std::string>("virtual_laser_cloud_topic_", "virtual_laser/cloud");
+        this->get_parameter_or<std::string>("virtual_laser_cloud_topic_", virtual_laser_cloud_topic_, "virtual_laser/cloud");
+        this->declare_parameter<std::string>("virtual_laser_cloud_frame_id_", "virtual_laser_frame");
+        this->get_parameter_or<std::string>("virtual_laser_cloud_frame_id_", virtual_laser_cloud_frame_id_, "virtual_laser_frame");
     }
 
 private:
+    std::string front_left_laser_scan_topic_;
+    std::string front_right_laser_scan_topic_;
+    std::string rear_left_laser_scan_topic_;
+    std::string rear_right_laser_scan_topic_;
+
+    std::string virtual_laser_cloud_topic_;
+    std::string virtual_laser_cloud_frame_id_;
 };
 
 int main(int argc, char *argv[])
