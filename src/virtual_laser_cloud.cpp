6210d65699e4fd1643323381b57b0d57da8107a4
Daniel Bacsur <contact@danielbacsur.com>
2024-03-04 16:23:15 +0100

    feat: add laser scan subscriptions and callbacks

--- src/virtual_laser_cloud.cpp
+++ src/virtual_laser_cloud.cpp
@@ -1,7 +1,10 @@
 #include <memory>
+#include <functional>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include "sensor_msgs/msg/point_cloud2.hpp"
 
 class VirtualLaserCloud : public rclcpp::Node
 {
@@ -21,9 +24,38 @@ public:
         this->get_parameter_or<std::string>("virtual_laser_cloud_topic_", virtual_laser_cloud_topic_, "virtual_laser/cloud");
         this->declare_parameter<std::string>("virtual_laser_cloud_frame_id_", "virtual_laser_frame");
         this->get_parameter_or<std::string>("virtual_laser_cloud_frame_id_", virtual_laser_cloud_frame_id_, "virtual_laser_frame");
+
+        front_left_laser_scan_subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+            front_left_laser_scan_topic_, 10, std::bind(&VirtualLaserCloud::front_left_laser_scan_callback, this, std::placeholders::_1));
+        front_right_laser_scan_subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+            front_right_laser_scan_topic_, 10, std::bind(&VirtualLaserCloud::front_right_laser_scan_callback, this, std::placeholders::_1));
+        rear_left_laser_scan_subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+            rear_left_laser_scan_topic_, 10, std::bind(&VirtualLaserCloud::rear_left_laser_scan_callback, this, std::placeholders::_1));
+        rear_right_laser_scan_subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+            rear_right_laser_scan_topic_, 10, std::bind(&VirtualLaserCloud::rear_right_laser_scan_callback, this, std::placeholders::_1));
     }
 
 private:
+    void front_left_laser_scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
+    {
+        front_left_laser_scan_ = msg;
+    }
+
+    void front_right_laser_scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
+    {
+        front_right_laser_scan_ = msg;
+    }
+
+    void rear_left_laser_scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
+    {
+        rear_left_laser_scan_ = msg;
+    }
+
+    void rear_right_laser_scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
+    {
+        rear_right_laser_scan_ = msg;
+    }
+
     std::string front_left_laser_scan_topic_;
     std::string front_right_laser_scan_topic_;
     std::string rear_left_laser_scan_topic_;
@@ -31,6 +63,16 @@ private:
 
     std::string virtual_laser_cloud_topic_;
     std::string virtual_laser_cloud_frame_id_;
+
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr front_left_laser_scan_subscription_;
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr front_right_laser_scan_subscription_;
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr rear_left_laser_scan_subscription_;
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr rear_right_laser_scan_subscription_;
+
+    sensor_msgs::msg::LaserScan::SharedPtr front_left_laser_scan_;
+    sensor_msgs::msg::LaserScan::SharedPtr front_right_laser_scan_;
+    sensor_msgs::msg::LaserScan::SharedPtr rear_left_laser_scan_;
+    sensor_msgs::msg::LaserScan::SharedPtr rear_right_laser_scan_;
 };
 
 int main(int argc, char *argv[])
