5a13fcf52f6df36f95ef6d9057f74da6c6897480
Daniel Bacsur <contact@danielbacsur.com>
2024-03-05 20:03:23 +0100

    refactor: update virtual laser scan topic and variable names

--- src/virtual_laser_scan.cpp
+++ src/virtual_laser_scan.cpp
@@ -15,10 +15,10 @@ public:
         this->get_parameter_or<std::string>("virtual_laser_scan_topic", virtual_laser_scan_topic_, "virtual_laser/scan");
         this->declare_parameter<std::string>("virtual_laser_scan_frame_id", "virtual_laser_frame");
         this->get_parameter_or<std::string>("virtual_laser_scan_frame_id", virtual_laser_scan_frame_id_, "virtual_laser_frame");
-        this->declare_parameter<std::string>("virtual_laser_cloud_topic", "virtual_laser/cloud");
-        this->get_parameter_or<std::string>("virtual_laser_cloud_topic", virtual_laser_cloud_topic_, "virtual_laser/cloud");
-        // this->declare_parameter<std::string>("virtual_laser_cloud_frame_id", "virtual_laser_frame");
-        // this->get_parameter_or<std::string>("virtual_laser_cloud_frame_id", virtual_laser_cloud_frame_id_, "virtual_laser_frame");
+        this->declare_parameter<std::string>("virtual_point_cloud_topic", "virtual_laser/cloud");
+        this->get_parameter_or<std::string>("virtual_point_cloud_topic", virtual_point_cloud_topic_, "virtual_laser/cloud");
+        // this->declare_parameter<std::string>("virtual_point_cloud_frame_id", "virtual_laser_frame");
+        // this->get_parameter_or<std::string>("virtual_point_cloud_frame_id", virtual_point_cloud_frame_id_, "virtual_laser_frame");
 
         this->declare_parameter<float>("virtual_laser_scan_angle_min", -M_PI);
         this->get_parameter_or<float>("virtual_laser_scan_angle_min", virtual_laser_scan_angle_min_, -M_PI);
@@ -35,16 +35,16 @@ public:
         this->declare_parameter<float>("virtual_laser_scan_range_max", 20.0);
         this->get_parameter_or<float>("virtual_laser_scan_range_max", virtual_laser_scan_range_max_, 20.0);
 
-        virtual_laser_cloud_subscription_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
-            virtual_laser_cloud_topic_, 10, std::bind(&VirtualLaserScan::virtual_laser_cloud_callback, this, std::placeholders::_1));
+        virtual_point_cloud_subscription_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
+            virtual_point_cloud_topic_, 10, std::bind(&VirtualLaserScan::virtual_point_cloud_callback, this, std::placeholders::_1));
 
         virtual_laser_scan_publisher_ = this->create_publisher<sensor_msgs::msg::LaserScan>(virtual_laser_scan_topic_, 10);
     }
 
 private:
-    void virtual_laser_cloud_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
+    void virtual_point_cloud_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
     {
-        virtual_laser_cloud_ = msg;
+        virtual_point_cloud_ = msg;
         update();
     }
 
@@ -67,7 +67,7 @@ private:
 
         virtual_laser_scan->ranges.assign(ranges, std::numeric_limits<double>::infinity());
 
-        for (sensor_msgs::PointCloud2ConstIterator<float> x(*virtual_laser_cloud_, "x"), y(*virtual_laser_cloud_, "y"), z(*virtual_laser_cloud_, "z"); x != x.end(); ++x, ++y, ++z)
+        for (sensor_msgs::PointCloud2ConstIterator<float> x(*virtual_point_cloud_, "x"), y(*virtual_point_cloud_, "y"), z(*virtual_point_cloud_, "z"); x != x.end(); ++x, ++y, ++z)
         {
 
             if (std::isnan(*x) || std::isnan(*y) || std::isnan(*z))
@@ -123,8 +123,8 @@ private:
 
     std::string virtual_laser_scan_topic_;
     std::string virtual_laser_scan_frame_id_;
-    std::string virtual_laser_cloud_topic_;
-    // std::string virtual_laser_cloud_frame_id_;
+    std::string virtual_point_cloud_topic_;
+    // std::string virtual_point_cloud_frame_id_;
 
     float virtual_laser_scan_angle_min_;
     float virtual_laser_scan_angle_max_;
@@ -134,10 +134,10 @@ private:
     float virtual_laser_scan_range_min_;
     float virtual_laser_scan_range_max_;
 
-    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr virtual_laser_cloud_subscription_;
+    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr virtual_point_cloud_subscription_;
     rclcpp::Publisher<sensor_msgs::msg::LaserScan>::SharedPtr virtual_laser_scan_publisher_;
 
-    sensor_msgs::msg::PointCloud2::SharedPtr virtual_laser_cloud_;
+    sensor_msgs::msg::PointCloud2::SharedPtr virtual_point_cloud_;
 };
 
 int main(int argc, char *argv[])
